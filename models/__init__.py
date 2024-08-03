564b2cce7867be16a1d20658dff8559ab300c948
渡渡鸟 <1565282301@qq.com>
2024-08-03 18:39:01 +0800

    update init

--- models/__init__.py
+++ models/__init__.py
@@ -1,65 +1,10 @@
-# -*- coding: utf-8 -*-
-# @Auther   : Mingsong Li (lms-07)
-# @Time     : 2023-Jul
-# @Address  : Time Lab @ SDU
-# @FileName : __init__.py
-# @Project  : LGTEUN (Pan-sharpening), IJCAI 2023
-
 from .base.builder import MODELS
 
-from .unlg_former import UnlgFormer
-from .MDCUN import MDCUN
-from .MutInf import MutInf
-from .SFIIN import SFIIN
-from .lightnet import lightnet
-from .INNT import INNT
-from .panformer import PanFormer
-
-from .GSA import GSA
-from .SFIM import SFIM
-from .Wavelet import Wavelet
-
-from .PanFlowNet import PanFlowNet
-from .MySFIIN import MySFIIN
-from .GPPNN import GPPNN
-from .PyDDN import PyDDN
-from .Brovey import Brovey
-from .Bicubic import Bicubic
-from .SRPPNN import SRPPNN
-from .PanNet import PanNet
-from .DuCoFPan import DuCoFPan
-from .PNN import PNN
-from .DifPan import DifPan
-from .MMNet import MMNet
-from .MSDCNN import MSDCNN
-from .IHS import IHS
-from .LGTEUNM import LGTEUNM
-from .MSDDN import MSDDN
-from .WINet import WINet
-from .PanMamba import PanMamba
-from .MyUnfold import MyUnfold
-from .HSIT import HSIT
-from .MyDualMamba import MyDualMamba
-from .MySFIIN2 import MySFIIN2
-from .DISPNet import DISPNet
-from .MyMambaIR import MyMambaIR
 from .SDMSPan import SDMSPan
-from .LGTEUNPAN import LGTEUNPAN
-from .PAPS import PAPS
-from .MSMambaIRCross import MSMambaIRCross
-from .MSMambaIR1Down import MSMambaIR1Down
-from .MSMambaIRBICUBIC import MSMambaIRBICUBIC
-from .MSMambaIR_WV2 import MSMambaIR_WV2
-from .MSMambaIRCNN import MSMambaIRCNN
-# __all__ = [
-#     'MODELS', 'PanFormer', 'UnlgFormer', 'MDCUN', 'INNT', 'MutInf', 'MMNet', 'GPPNN', 'lightnet', 'ADKNet', 'SFIIN',
-#     'LACNet', 'SFIM', 'GSA'
-# ]
+
+
 
 __all__ = [
-    'MODELS', 'UnlgFormer', 'MDCUN', 'MutInf', 'SFIIN', 'lightnet', 'INNT', 'PanFormer', 'Wavelet', 'SFIM', 'GSA', 'PanFlowNet', 'MySFIIN', 'GPPNN',
-    'PyDDN', 'Brovey', 'Bicubic', 'SRPPNN', 'PanNet', 'DuCoFPan', 'PNN', 'DifPan', 'MMNet', 'MSDCNN', 'IHS', 'LGTEUNM', 'MSDDN',
-    'WINet', 'PanMamba', 'MyUnfold', 'HSIT', 'MyDualMamba', 'MySFIIN2', 'DISPNet', 'MyMambaIR', 'SDMSPan', 'LGTEUNPAN', 'PAPS',
-    'MSMambaIRCross', 'MSMambaIR1Down', 'MSMambaIRBICUBIC', 'MSMambaIR_WV2', 'MSMambaIRCNN'
+    'MODELS',  'SDMSPan'
 ]
                
