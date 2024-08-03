0562b026e431966b6f32b79955ca84bddc3eb16e
zhaomengjiao123 <61865793+zhaomengjiao123@users.noreply.github.com>
2024-08-03 18:35:09 +0800

    Update __init__.py

--- Dataset/__init__.py
+++ Dataset/__init__.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-# @Auther   : Mingsong Li (lms-07)
-# @Time     : 2023-May
-# @Address  : Time Lab @ SDU
-# @FileName : __init__.py.py
-# @Project  : LGTEUN (Pan-sharpening), IJCAI 2023
 
 from .ps_dataset import PSDataset
 from .builder import DATASETS
